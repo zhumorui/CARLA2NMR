f0f43c0ad34ee436131d36c5890ed3f5f169ac10
zhumorui <11309989349@qq.com>
2024-05-06 10:38:12 +0800

    LOAD_FILE_ITEM

--- src/gui/app.py
+++ src/gui/app.py
@@ -1,10 +1,3 @@
-# ----------------------------------------------------------------------------
-# -                        Open3D: www.open3d.org                            -
-# ----------------------------------------------------------------------------
-# Copyright (c) 2018-2023 www.open3d.org
-# SPDX-License-Identifier: MIT
-# ----------------------------------------------------------------------------
-
 import open3d as o3d
 import open3d.visualization.gui as gui # type: ignore
 import open3d.visualization.rendering as rendering # type: ignore
@@ -12,30 +5,20 @@ import platform
 import random
 import threading
 import time
+from src.model.model import Model
 
 isMacOS = (platform.system() == "Darwin")
 
-
-# This example shows two methods of adding geometry to an existing scene.
-# 1) add via a UI callback (in this case a menu, but a button would be similar,
-#    you would call `button.set_on_clicked(self.on_menu_sphere_)` when
-#    configuring the button. See `on_menu_sphere()`.
-# 2) add asynchronously by polling from another thread. GUI functions must be
-#    called from the UI thread, so use Application.post_to_main_thread().
-#    See `on_menu_random()`.
-# Running the example will show a simple window with a Debug menu item with the
-# two different options. The second method will add random spheres for
-# 20 seconds, during which time you can be interacting with the scene, rotating,
-# etc.
 class CARLA2NMR_App:
-    MENU_SPHERE = 1
-    MENU_RANDOM = 2
-    MENU_QUIT = 3
+    MENU_QUIT = 1
+    MENU_LOAD_FILE = 2
+    MENU_SHOW_CAMERA = 3
+    MENU_SHOW_IMAGE = 4
 
     def __init__(self):
-        self._id = 0
+        self.model = None
         self.window = gui.Application.instance.create_window(
-            "Add Spheres Example", 1024, 768)
+            "CARLA2NMR Viewer", 1024, 768)
         self.scene = gui.SceneWidget()
         self.scene.scene = rendering.Open3DScene(self.window.renderer)
         self.scene.scene.set_background([1, 1, 1, 1])
@@ -44,10 +27,6 @@ class CARLA2NMR_App:
             [1, 1, 1],  # color
             100000)  # intensity
         self.scene.scene.scene.enable_sun_light(True)
-        bbox = o3d.geometry.AxisAlignedBoundingBox([-10, -10, -10],
-                                                   [10, 10, 10])
-        self.scene.setup_camera(60, bbox, [0, 0, 0])
-
         self.window.add_child(self.scene)
 
         # The menu is global (because the macOS menu is global), so only create
@@ -56,12 +35,13 @@ class CARLA2NMR_App:
             if isMacOS:
                 app_menu = gui.Menu()
                 app_menu.add_item("Quit", CARLA2NMR_App.MENU_QUIT)
-            debug_menu = gui.Menu()
-            debug_menu.add_item("Add Sphere", CARLA2NMR_App.MENU_SPHERE)
-            debug_menu.add_item("Add Random Spheres", CARLA2NMR_App.MENU_RANDOM)
+            file_menu = gui.Menu()
+            file_menu.add_item("Open Dir", CARLA2NMR_App.MENU_LOAD_FILE)
+            view_menu = gui.Menu()
+            view_menu.add_item("Show Camera", CARLA2NMR_App.MENU_SHOW_CAMERA)
             if not isMacOS:
-                debug_menu.add_separator()
-                debug_menu.add_item("Quit", CARLA2NMR_App.MENU_QUIT)
+                file_menu.add_separator()
+                file_menu.add_item("Quit", CARLA2NMR_App.MENU_QUIT)
 
             menu = gui.Menu()
             if isMacOS:
@@ -69,59 +49,51 @@ class CARLA2NMR_App:
                 # (in our case, probably "Python"), regardless of what we call
                 # it. This is the application menu, and it is where the
                 # About..., Preferences..., and Quit menu items typically go.
-                menu.add_menu("Example", app_menu)
-                menu.add_menu("Debug", debug_menu)
+                menu.add_menu("Python", app_menu) 
+                menu.add_menu("File", file_menu)
+                menu.add_menu("View", view_menu)
             else:
-                menu.add_menu("Debug", debug_menu)
+                menu.add_menu("File", file_menu)
+                menu.add_menu("View", view_menu)
             gui.Application.instance.menubar = menu
 
         # The menubar is global, but we need to connect the menu items to the
         # window, so that the window can call the appropriate function when the
         # menu item is activated.
-        self.window.set_on_menu_item_activated(CARLA2NMR_App.MENU_SPHERE,
-                                               self._on_menu_sphere)
-        self.window.set_on_menu_item_activated(CARLA2NMR_App.MENU_RANDOM,
-                                               self._on_menu_random)
         self.window.set_on_menu_item_activated(CARLA2NMR_App.MENU_QUIT,
                                                self._on_menu_quit)
-
-    def add_sphere(self):
-        self._id += 1
-        mat = rendering.MaterialRecord()
-        mat.base_color = [
-            random.random(),
-            random.random(),
-            random.random(), 1.0
-        ]
-        mat.shader = "defaultLit"
-        sphere = o3d.geometry.TriangleMesh.create_sphere(0.5)
-        sphere.compute_vertex_normals()
-        sphere.translate([
-            10.0 * random.uniform(-1.0, 1.0), 10.0 * random.uniform(-1.0, 1.0),
-            10.0 * random.uniform(-1.0, 1.0)
-        ])
-        self.scene.scene.add_geometry("sphere" + str(self._id), sphere, mat)
-
-    def _on_menu_sphere(self):
-        # GUI callbacks happen on the main thread, so we can do everything
-        # normally here.
-        self.add_sphere()
-
-    def _on_menu_random(self):
-        # This adds spheres asynchronously. This pattern is useful if you have
-        # data coming in from another source than user interaction.
-        def thread_main():
-            for _ in range(0, 20):
-                # We can only modify GUI objects on the main thread, so we
-                # need to post the function to call to the main thread.
-                gui.Application.instance.post_to_main_thread(
-                    self.window, self.add_sphere)
-                time.sleep(1)
-
-        threading.Thread(target=thread_main).start()
+        self.window.set_on_menu_item_activated(CARLA2NMR_App.MENU_LOAD_FILE,
+                                               self._on_menu_filedlg)
+        self.window.set_on_menu_item_activated(CARLA2NMR_App.MENU_SHOW_CAMERA,
+                                                self._on_menu_show_camera)
+        # self.window.set_on_menu_item_activated(CARLA2NMR_App.MENU_SHOW_IMAGE,
+        #                                         self._on_menu_show_image)
 
     def _on_menu_quit(self):
         gui.Application.instance.quit()
 
+    def _on_menu_filedlg(self):
+        filedlg = gui.FileDialog(gui.FileDialog.OPEN_DIR, "Select dir",
+                                 self.window.theme)
+        filedlg.set_on_cancel(self._on_filedlg_cancel)
+        filedlg.set_on_done(self._on_filedlg_done)
+        self.window.show_dialog(filedlg)
 
+    def _on_filedlg_cancel(self):
+        self.window.close_dialog()
 
+    def _on_filedlg_done(self, path):
+        self.model = Model(path, ext=".txt")
+        print(f"Load model successfully!\n{self.model}")
+        # self._on_menu_show_camera()
+        self.window.close_dialog()
+
+    def _on_menu_show_camera(self):
+        frames = self.model.add_cameras()
+        self.scene.scene.clear_geometry()
+        for id, cam in zip(range(10), frames[:10]):
+            self.scene.scene.add_model(f"camera_{id}", cam)
+        
+
+    def _on_menu_show_image(self):
+        pass
