969e4b3da34b3fddf6ce05499f516b88988c192e
zhumorui <11309989349@qq.com>
2024-05-06 10:39:38 +0800

    Draw cameras

--- src/model/model.py
+++ src/model/model.py
@@ -5,11 +5,12 @@ import argparse
 import numpy as np
 import open3d as o3d
 from src.utils.colmap.python.read_write_model import read_model, write_model, qvec2rotmat, rotmat2qvec
-
+from src.utils.vis_utils import draw_camera
 
 class Model:
     def __init__(self,model_path, ext=None):
         self.cameras = []
+        self.poses = []
         self.images = []
         self.points3D = []
         self.masks = []
@@ -20,6 +21,7 @@ class Model:
 
     def __str__(self):
         model_info = f"num_cameras: {len(self.cameras)}\n"
+        model_info += f"num_poses: {len(self.poses)}\n"
         model_info += f"num_images: {len(self.images)}\n"
         model_info += f"num_points3D: {len(self.points3D)}\n"
         model_info += f"num_masks: {len(self.masks)}\n"
@@ -30,13 +32,63 @@ class Model:
 
 
     def read_model(self, path, ext=".txt"):
-        self.cameras, self.images, self.points3D = read_model(os.path.join(path, "sparse/0"), ext)
+        self.cameras, images_info, self.points3D = read_model(os.path.join(path, "sparse/0"), ext)
+        self.images = [os.path.join(path, "images", img.name) for img in images_info.values()]
+        self.poses = [(img.qvec, img.tvec, img.camera_id) for img in images_info.values()]
         self.masks = sorted(glob(os.path.join(path, "mask", "*.png")))
         self.depth16 = sorted(glob(os.path.join(path, "depth_16", "*.png")))
         self.lidars = sorted(glob(os.path.join(path, "lidar", "*.ply")))
 
+    # add camera trajectories, add image textures if available
+    def add_cameras(self, scale=1):
+        frames = []
+        for img, pose in zip(self.images, self.poses):
+            # rotation
+            R = qvec2rotmat(pose[0])
 
+            # translation
+            t = pose[1]
+
+            # invert
+            t = -R.T @ t
+            R = R.T
+
+            # intrinsics
+            cam = self.cameras[pose[2]]
+
+            if cam.model in ("SIMPLE_PINHOLE", "SIMPLE_RADIAL", "RADIAL"):
+                fx = fy = cam.params[0]
+                cx = cam.params[1]
+                cy = cam.params[2]
+            elif cam.model in (
+                "PINHOLE",
+                "OPENCV",
+                "OPENCV_FISHEYE",
+                "FULL_OPENCV",
+            ):
+                fx = cam.params[0]
+                fy = cam.params[1]
+                cx = cam.params[2]
+                cy = cam.params[3]
+            else:
+                raise Exception("Camera model not supported")
+
+            # intrinsics
+            K = np.identity(3)
+            K[0, 0] = fx
+            K[1, 1] = fy
+            K[0, 2] = cx
+            K[1, 2] = cy
+
+            # create axis, plane and pyramed geometries that will be drawn
+            cam_model = draw_camera(K, R, t, cam.width, cam.height, scale, img_path=img)
+            frames.extend(cam_model)
+        
+        return frames
+    
 if __name__ == "__main__":
-    model = Model('/home/morin/ZHITAI_SSD/projects/monopointcloud/_out/sequences/04_23_15_38_23/carla_12_20_rgb_1_1/', ext=".txt")
+    model = Model('/Users/morin/carla_12_20_rgb_1_1/', ext=".txt")
+    cameras = model.add_cameras()
     print(model)
+    
 
---
+++ src/utils/vis_utils.py
@@ -0,0 +1,109 @@
+import numpy as np
+import open3d as o3d
+import cv2
+
+def draw_camera(K, R, t, w, h, scale=1, color=[0.8, 0.2, 0.8], img_path=None):
+    """Create axis, plane and pyramed geometries in o3d format.
+    :param K: calibration matrix (camera intrinsics)
+    :param R: rotation matrix
+    :param t: translation
+    :param w: image width
+    :param h: image height
+    :param scale: camera model scale
+    :param color: color of the image plane and pyramid lines
+    :param img_path: path to the image file
+    :return: camera model geometries (axis, plane and pyramid)
+    """
+
+    # intrinsics
+    K = K.copy() / scale
+    Kinv = np.linalg.inv(K)
+
+    # 4x4 transformation
+    T = np.column_stack((R, t))
+    T = np.vstack((T, (0, 0, 0, 1)))
+
+    # axis
+    axis = o3d.geometry.TriangleMesh.create_coordinate_frame(
+        size=0.5 * scale
+    )
+    
+    axis.transform(T)
+
+    # points in pixel
+    points_pixel = [
+        [0, 0, 0],
+        [0, 0, 1],
+        [w, 0, 1],
+        [0, h, 1],
+        [w, h, 1],
+    ]
+
+    # pixel to camera coordinate system
+    points = [Kinv @ p for p in points_pixel]
+
+
+    
+    img = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
+
+    # image plane
+    width = abs(points[1][0]) + abs(points[3][0])
+    height = abs(points[1][1]) + abs(points[3][1])
+
+    w, h, = width, height
+
+    # Normalize the vertex positions by the maximum dimension to maintain aspect ratio
+    vertices = np.array([
+        [0, 0, 0],   # Bottom-left
+        [w, 0, 0],   # Bottom-right
+        [w, h, 0],   # Top-right
+        [0, h, 0]    # Top-left
+    ])  # Normalize to keep aspect ratio
+
+    faces = np.array([
+        [0, 1, 2],  # Lower triangle
+        [2, 3, 0],  # Upper triangle
+    ])
+    
+    # Set UV coordinates
+    v_uv = np.array([
+        [0, 0],  # UV for Top-left
+        [1, 0],  # UV for Top-right
+        [1, 1],  # UV for Bottom-right
+        [0, 1]   # UV for Bottom-left
+    ])
+
+
+    # Correct faces' UV indexing, each face needs 3 UV coordinates, one for each vertex
+    face_uvs = np.array([
+        [0, 1, 2],  # UVs for the first triangle
+        [2, 3, 0]   # UVs for the second triangle
+    ])
+
+    # Create the mesh
+    mesh = o3d.geometry.TriangleMesh(o3d.utility.Vector3dVector(vertices), o3d.utility.Vector3iVector(faces))
+    mesh.textures = [o3d.geometry.Image(img)]
+    mesh.triangle_uvs = o3d.utility.Vector2dVector(v_uv[face_uvs.flatten()])
+    mesh.triangle_material_ids = o3d.utility.IntVector([0] * len(faces))
+        
+    mesh.translate([points[1][0], points[1][1], scale])
+    mesh.transform(T)
+
+
+    # pyramid
+    points_in_world = [(R @ p + t) for p in points]
+    lines = [
+        [0, 1],
+        [0, 2],
+        [0, 3],
+        [0, 4],
+    ]
+    colors = [color for i in range(len(lines))]
+    line_set = o3d.geometry.LineSet(
+        points=o3d.utility.Vector3dVector(points_in_world),
+        lines=o3d.utility.Vector2iVector(lines),
+    )
+    line_set.colors = o3d.utility.Vector3dVector(colors)
+
+    # return as list in o3d format
+    return [axis, mesh, line_set]
